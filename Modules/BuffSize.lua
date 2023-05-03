e3012d5b2f54f03778eaba9676b9d94032e90f11
Slothpala <slothpala@gmail.com>
2023-05-03 12:16:16 +0200

    added 4th buff slot

--- Modules/BuffSize.lua
+++ Modules/BuffSize.lua
@@ -7,8 +7,9 @@ function BuffSize:OnEnable()
     local width  = RaidFrameSettings.db.profile.MinorModules.BuffSize.width
     local height = RaidFrameSettings.db.profile.MinorModules.BuffSize.height
     local function UpdateAllCallback(frame)
-        for i = 1,3 do
+        for i = 1,4 do
             frame.buffFrames[i]:SetSize(width, height)
+            if frame.buffFrames[i].icon then frame.buffFrames[i].icon:SetVertexColor(0,0,0) end
         end
     end
     RaidFrameSettings:RegisterOnUpdateAll(UpdateAllCallback)

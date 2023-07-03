0e08fe852c66f304a1aeacc7a9483a599283b806
Slothpala <slothpala@gmail.com>
2023-07-03 12:41:19 +0200

    OnDisable function added for DebuffSize

--- Modules/DebuffSize.lua
+++ Modules/DebuffSize.lua
@@ -29,6 +29,17 @@ function DebuffSize:OnEnable()
     RaidFrameSettings:RegisterUpdateDebuffFrame(UtilSetDebuff_Callback)
 end
 
+--parts of this code are from FrameXML/CompactUnitFrame.lua
 function DebuffSize:OnDisable()
     UtilSetDebuff_Callback = function() end
+    local restoreDebuffFrames = function(frame)
+        local frameWidth = frame:GetWidth()
+        local frameHeight = frame:GetHeight()
+        local componentScale = min(frameWidth / NATIVE_UNIT_FRAME_HEIGHT, frameWidth / NATIVE_UNIT_FRAME_WIDTH)
+        local buffSize = math.min(15, 11 * componentScale)
+        for i=1,#frame.debuffFrames do  
+            frame.debuffFrames[i]:SetSize(buffSize, buffSize)
+        end
+    end
+    RaidFrameSettings:IterateRoster(restoreDebuffFrames)
 end

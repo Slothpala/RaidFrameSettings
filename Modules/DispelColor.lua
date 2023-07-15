97848e4904d41abfd006666d89165f3670eea453
Slothpala <slothpala@gmail.com>
2023-07-15 17:16:56 +0200

    Rewritten Dispel Color module

--- Modules/DispelColor.lua
+++ Modules/DispelColor.lua
@@ -1,6 +1,8 @@
 --[[
     Created by Slothpala
 --]]
+local DispelColor = RaidFrameSettings:NewModule("DispelColor")
+local LGF = LibStub("LibGetFrame-1.0")
 --lua speed reference
 local ipairs = ipairs
 local pairs = pairs
@@ -9,79 +11,63 @@ local UnitIsConnected = UnitIsConnected
 local UnitIsDead = UnitIsDead
 local UnitIsTapDenied = UnitIsTapDenied
 local UnitInVehicle = UnitInVehicle
---
-local DispelColor = RaidFrameSettings:NewModule("DispelColor")
-local Roster = {}
---[[
-    Roster = {
-        unit = {
-            frame = frame,
-            debuffs = {
-                aura.auraInstanceID = aura.dispelName,
-                aura.auraInstanceID_N = aura.dispelName_N,
-                ...
-            },
-        },
-    }
---]]
+local UnitDebuff = UnitDebuff
+--locals
+local eligibleUnit = {}
+eligibleUnit["player"] = true
+for i=1,4 do eligibleUnit["party"..i] = true end
+for i=1,40 do eligibleUnit["raid"..i] = true end
+local DebuffState = {}
 local debuffTypeColor = {}
+local updateColor --function defined in OnEnable()
 
-local restoreStatusBarColor --function defined in OnEnable()
+local function isValidCompactFrame(frame) 
+    if frame:IsForbidden() then return end
+    local frameName = frame:GetName()
+    if frameName and frameName:match("Member") then 
+        return true
+    end
+    return false
+end
 
 local function setStatusBarToDebuffColor(unit, debuffType)
-    if UnitIsTapDenied(unit) or UnitInVehicle(unit) then 
-        return
+    local frame = LGF.GetUnitFrame(unit)
+    if frame and isValidCompactFrame(frame) then
+        frame.healthBar:SetStatusBarColor(debuffTypeColor[debuffType].r,debuffTypeColor[debuffType].g,debuffTypeColor[debuffType].b)
     end
-    Roster[unit].frame.healthBar:SetStatusBarColor(debuffTypeColor[debuffType].r,debuffTypeColor[debuffType].g,debuffTypeColor[debuffType].b)
+end
+
+local function restoreStatusBarColor(unit)
+    local frame = LGF.GetUnitFrame(unit)
+    if frame and isValidCompactFrame(frame) then
+        updateColor(frame)
+    end
 end
 
 local function checkDebuffs(unit)
-    for _,debuffType in pairs(Roster[unit].debuffs) do
-        if debuffType then
-            setStatusBarToDebuffColor(unit, debuffType)
-            return
+    local index = 0
+    local debuff = nil
+    while(true) do
+        index = index +1
+        local _, _, _, debuffType = UnitDebuff(unit, index, "HARMFUL|RAID")
+        if not debuffType then 
+            break 
         end
+        debuff = debuffType
+        break
     end
-    Roster[unit].debuffs = {}
-    restoreStatusBarColor(unit)
-end
-
-local function processAurasFull(unit)
-    Roster[unit].debuffs = {}
-    local function HandleAura(aura)
-        Roster[unit].debuffs[aura.auraInstanceID] = aura.dispelName
-        setStatusBarToDebuffColor(unit, aura.dispelName)   
-    end
-    AuraUtil.ForEachAura(unit, "HARMFUL|RAID", nil, HandleAura, true)    
-end
-
-local function processAurasIncremental(unit, unitAuraUpdateInfo)
-    if unitAuraUpdateInfo.addedAuras ~= nil then
-        for _, aura in ipairs(unitAuraUpdateInfo.addedAuras) do
-            if aura.isRaid and aura.isHarmful then
-                Roster[unit].debuffs[aura.auraInstanceID] = aura.dispelName          
-            end
-        end
-    end
-    if unitAuraUpdateInfo.removedAuraInstanceIDs ~= nil then
-        for _, auraInstanceID in ipairs(unitAuraUpdateInfo.removedAuraInstanceIDs) do
-            if Roster[unit].debuffs[auraInstanceID] then
-                Roster[unit].debuffs[auraInstanceID] = nil
-            end
-        end
-    end
-    checkDebuffs(unit)
-end
-
-function DispelColor:OnUnitAura(_,unit,unitAuraUpdateInfo)
-    if not Roster[unit] then
-        return
-    end
-    if unitAuraUpdateInfo == nil or unitAuraUpdateInfo.isFullUpdate then
-        processAurasFull(unit)
-        return
+    if debuff then 
+        DebuffState[unit] = true
+        setStatusBarToDebuffColor(unit, debuff)
     else
-        processAurasIncremental(unit, unitAuraUpdateInfo)
+        DebuffState[unit] = false
+        restoreStatusBarColor(unit)
+    end
+end
+
+function DispelColor:OnUnitAura(_,unit)
+    if eligibleUnit[unit] then
+        checkDebuffs(unit)
     end
 end
 
@@ -96,70 +82,51 @@ function DispelColor:OnEnable()
         --static color
         if RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 3 then 
             local statusBarColor = RaidFrameSettings.db.profile.HealthBars.Colors.statusbar
-            restoreStatusBarColor = function(unit)
-                Roster[unit].frame.healthBar:SetStatusBarColor(statusBarColor.r,statusBarColor.g,statusBarColor.b)
+            updateColor = function(frame)
+                frame.healthBar:SetStatusBarColor(statusBarColor.r,statusBarColor.g,statusBarColor.b)
             end
-            local function UpdateHealthColor(frame)
-                if Roster[frame.unit] then
-                    checkDebuffs(frame.unit)
-                else
-                    frame.healthBar:SetStatusBarColor(statusBarColor.r,statusBarColor.g,statusBarColor.b) 
-                end
-            end
-            RaidFrameSettings:RegisterOnUpdateHealthColor(UpdateHealthColor)
         --green color
-        elseif 
-            RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 2 then 
-                restoreStatusBarColor = function(unit)
-                    Roster[unit].frame.healthBar:SetStatusBarColor(0,1,0)
-                end
+        elseif RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 2 then 
+            updateColor = function(frame)
+                frame.healthBar:SetStatusBarColor(0,1,0)
+            end
         --class color
-        elseif 
-            RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 1 then 
-                restoreStatusBarColor = function(unit)
-                    local _, englishClass = UnitClass(unit)
-                    local r,g,b = GetClassColor(englishClass)
-                    Roster[unit].frame.healthBar:SetStatusBarColor(r,g,b)
-                end
+        elseif RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 1 then 
+            updateColor = function(frame)
+                local _, englishClass = UnitClass(frame.unit) 
+                local r,g,b = GetClassColor(englishClass)
+                frame.healthBar:SetStatusBarColor(r,g,b)
+            end
         end
     else
         if C_CVar.GetCVar("raidFramesDisplayClassColor") == "0" then
-            restoreStatusBarColor = function(unit)
-                Roster[unit].frame.healthBar:SetStatusBarColor(0,1,0)
+            updateColor = function(frame)
+                frame.healthBar:SetStatusBarColor(0,1,0)
             end
         else
-            restoreStatusBarColor = function(unit)
-                local _, englishClass = UnitClass(unit)
+            updateColor = function(frame)
+                local _, englishClass = UnitClass(frame.unit)
                 local r,g,b = GetClassColor(englishClass)
-                Roster[unit].frame.healthBar:SetStatusBarColor(r,g,b)
+                frame.healthBar:SetStatusBarColor(r,g,b)
             end
         end
     end
-    --RaidFrameSettings:RegisterOnUpdateHealthColor(UpdateHealthColorCallback)
-    --LGF Callbacks
-    --added
-    local function OnFrameUnitAdded(frame, unit)
-        Roster[unit] = {}
-        Roster[unit].frame = frame
-        Roster[unit].debuffs = {}
-        processAurasFull(unit)
-        checkDebuffs(unit)
+    local UpdateHealthColor
+    if RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 3 then 
+        UpdateHealthColor = function(frame)
+            updateColor(frame)
+            if DebuffState[frame.unit] then
+                checkDebuffs(frame.unit)
+            end
+        end
+    else
+        UpdateHealthColor = function(frame)
+            if DebuffState[frame.unit] then
+                checkDebuffs(frame.unit)
+            end
+        end
     end
-    RaidFrameSettings:RegisterOnFrameUnitAdded(OnFrameUnitAdded)
-    --update
-    local function OnFrameUnitUpdate(frame, unit)
-        Roster[unit] = {}
-        Roster[unit].frame = frame
-        Roster[unit].debuffs = {}
-        processAurasFull(unit)
-        checkDebuffs(unit)
-    end
-    RaidFrameSettings:RegisterOnFrameUnitUpdate(OnFrameUnitUpdate)
-    --removed
-    local function OnFrameUnitRemoved(unit)
-        Roster[unit] = nil
-    end
-    RaidFrameSettings:RegisterOnFrameUnitRemoved(OnFrameUnitRemoved)
+    RaidFrameSettings:RegisterOnUpdateHealthColor(UpdateHealthColor)
 end
 
 function DispelColor:OnDisable()

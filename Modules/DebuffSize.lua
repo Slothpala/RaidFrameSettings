41f45fbd6bc6a123f7264e9c59f25e4ff7fa1e73
Slothpala <slothpala@gmail.com>
2023-07-17 03:14:36 +0200

    BossAuras will now respect custom size settings

--- Modules/DebuffSize.lua
+++ Modules/DebuffSize.lua
@@ -4,8 +4,6 @@
 local DebuffSize = RaidFrameSettings:NewModule("DebuffSize")
 local hooked = nil
 --Debuffframe size
-local debuffsize_increase = BOSS_DEBUFF_SIZE_INCREASE
-local minimum, maximum = min, max
 local SetSize = SetSize
 local IsForbidden = IsForbidden
 local UtilSetDebuff_Callback
@@ -16,13 +14,7 @@ function DebuffSize:OnEnable()
     local height = RaidFrameSettings.db.profile.MinorModules.DebuffSize.height
     UtilSetDebuff_Callback = function(debuffFrame, aura)
         if debuffFrame:IsForbidden() then return end
-        if aura and aura.isBossAura then
-            local boss_height = minimum(height + debuffsize_increase, debuffFrame.maxHeight)
-            local boss_width  = maximum(width, boss_height)
-            debuffFrame:SetSize(boss_width, boss_height)
-        else
-            debuffFrame:SetSize(width, height)
-        end
+        debuffFrame:SetSize(width, height)
     end
     if not hooked then
         hooksecurefunc("CompactUnitFrame_UtilSetDebuff", function(debuffFrame, aura) UtilSetDebuff_Callback(debuffFrame, aura) end)

eb25747177332954a0a70dd3cf078915123ee7c8
Slothpala <slothpala@gmail.com>
2023-11-07 18:27:27 +0100

    corpse added

--- Modules/DebuffHighlight.lua
+++ Modules/DebuffHighlight.lua
@@ -3,3 +3,19 @@ local RaidFrameSettings = addonTable.RaidFrameSettings
 
 local module = RaidFrameSettings:NewModule("DebuffHighlight")
 Mixin(module, addonTable.hooks)
+
+function module:OnEnable()
+
+end
+
+function module:OnDisable()
+
+end
+
+function module:Start()
+
+end
+
+function module:Stop()
+
+end

d2144f2eba7d9387e0e90067a4a474caf6c114a3
Slothpala <slothpala@gmail.com>
2023-11-08 15:21:02 +0100

    list updated

--- Modules/BleedDB.lua
+++ Modules/BleedDB.lua
@@ -1,25 +1,32 @@
 local _, addonTable = ...
-local Bleeds = addonTable.Bleeds
-
 --DF M+ Season 3
 --[[
     credit for m+ season 3 bleeds collection to this redditor:
     https://www.reddit.com/r/CompetitiveWoW/comments/17j082i/dispels_for_dragonflight_season_3/
 ]]--
 local Bleeds = {
+    --Atal'Dazar
+    [255814] = "Rending Maul",
+    --Black Rook Hold
     [225963] = "Bloodthirsty Leap",
     [197546] = "Brutal Glaive",
+    --Darkheart Thicket
     [196376] = "Grievous Tear",
     [225484] = "Grievous Rip",
     [200182] = "Festering Rip",
+    --Dawn of the Infinite: Lower
     [412285] = "Stonebolt",
+    --Dawn of the Infinite: Upper
     [418009] = "Serrated Arrows",
     [407120] = "Serrated Axe",
     [416258] = "Stonebolt",
     [407313] = "Shrapnel",
     [412505] = "Rending Cleave",
     [411700] = "Slobbering Bite",
+    --Waycrest Manor
     [271178] = "Ravaging Leap",
     [260741] = "Jagged Nettles",
     [264556] = "Tearing Strike",
 }
+
+addonTable.Bleeds = Bleeds

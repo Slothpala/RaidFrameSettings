2ca45be598887c4eec557ee74cef3cc599d0e4e9
Slothpala <slothpala@gmail.com>
2023-11-20 14:50:21 +0100

    making use of LibCanDispel library

--- Modules/DebuffHighlight.lua
+++ Modules/DebuffHighlight.lua
@@ -3,6 +3,7 @@ local RaidFrameSettings = addonTable.RaidFrameSettings
 
 local module = RaidFrameSettings:NewModule("DebuffHighlight")
 Mixin(module, addonTable.hooks)
+local LCD --LibCanDispel or custom defined in OnEnable
 
 local playerClass = select(2,UnitClass("player"))
 local SetStatusBarColor = SetStatusBarColor
@@ -21,7 +22,6 @@ local debuffColors = {
     Poison  = {r=0.0,g=0.6,b=0.0},
     Bleed   = {r=0.8,g=0.0,b=0.0},
 }
-local canCure = {}
 
 local Bleeds = addonTable.Bleeds
 local auraMap = {}
@@ -47,10 +47,10 @@ end
 local function updateAurasFull(frame)
     auraMap[frame] = {}
     local function HandleAura(aura)
-        if canCure[aura.dispelName] then
+        if aura.dispelName and LCD:CanDispel(aura.dispelName) then
             auraMap[frame][aura.auraInstanceID] = aura.dispelName
         end
-        if Bleeds[aura.spellId] and canCure["Bleed"] then 
+        if Bleeds[aura.spellId] and LCD:CanDispel("Bleed") then 
             auraMap[frame][aura.auraInstanceID] = "Bleed"
         end
     end
@@ -61,10 +61,10 @@ end
 local function updateAurasIncremental(frame, updateInfo)
     if updateInfo.addedAuras then
         for _, aura in pairs(updateInfo.addedAuras) do
-            if aura.isHarmful and canCure[aura.dispelName] then
+            if aura.isHarmful and aura.dispelName and LCD:CanDispel(aura.dispelName) then
                 auraMap[frame][aura.auraInstanceID] = aura.dispelName
             end
-            if Bleeds[aura.spellId] and canCure["Bleed"] then 
+            if Bleeds[aura.spellId] and LCD:CanDispel("Bleed") then 
                 auraMap[frame][aura.auraInstanceID] = "Bleed"
             end
         end
@@ -102,185 +102,6 @@ function module:HookFrame(frame)
     self:HookScript(frame, "OnHide", function(frame)
         blockColorUpdate[frame] = nil
     end)
-end
-
-function module:OnEnable()
-    self:SetUpdateHealthColor()
-    local dbObj = RaidFrameSettings.db.profile.DebuffHighlight
-    if dbObj.Config.operation_mode == 1 then
-        self:RegisterEvents()
-        self:UpdateCurable()
-    else
-        canCure.Curse = dbObj.Config.Curse
-        canCure.Disease = dbObj.Config.Disease
-        canCure.Magic = dbObj.Config.Magic
-        canCure.Poison = dbObj.Config.Poison
-        canCure.Bleed = dbObj.Config.Bleed
-    end
-    self:GetDebuffColors()
-    self:HookFunc("CompactUnitFrame_RegisterEvents", function(frame)
-        if frame.unit:match("na") then --this will exclude nameplates and arena
-            return
-        end
-        if not UnitIsPlayer(frame.unit) then --exclude pet/vehicle frame
-            return
-        end
-        self:HookFrame(frame)                          
-    end)   
-    local onUpdateHealthColor
-    if RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 3 then 
-        onUpdateHealthColor = function(frame) 
-            if blockColorUpdate[frame] then
-                updateColor(frame)
-            else
-                updateHealthColor(frame)
-            end
-        end
-    else
-        onUpdateHealthColor = function(frame) 
-            if blockColorUpdate[frame] then
-                updateColor(frame)
-            end
-        end
-    end
-    --[[
-        CompactUnitFrame_UpdateHealthColor checks the current healthbar color value and restores it to the designated color if it differs from it.
-        If this happens while the frame has a debuff color, we will need to update it again.
-    ]]--
-    self:HookFuncFiltered("CompactUnitFrame_UpdateHealthColor", onUpdateHealthColor)
-    RaidFrameSettings:IterateRoster(function(frame)
-        self:HookFrame(frame)
-        updateAurasFull(frame)
-    end)
-end
-
-function module:OnDisable()
-    self:DisableHooks()
-    self:UnregisterEvents()
-end
-
-function module:UpdateCurable()
-    canCure = {}
-    local dispelAbilities = {
-        ["DRUID"] = function()
-            if IsSpellKnown(2782) then --Remove Corruption
-                canCure.Curse = true
-                canCure.Poison = true
-            end
-            if IsSpellKnown(88423) then --Nature's Cure
-                canCure.Magic = true
-                if IsPlayerSpell(392378) then --Improved Nature's Cure
-                    canCure.Curse = true
-                    canCure.Poison = true
-                end
-            end
-        end,
-        ["MAGE"] = function()
-            if IsSpellKnown(475) then --Remove Curse
-                canCure.Curse = true
-            end
-        end,
-        ["MONK"] = function()
-            if IsSpellKnown(218164) then --Detox BM/WW
-                canCure.Poison = true
-                canCure.Disease = true
-            end
-            if IsSpellKnown(115450) then --Detox MW 
-                canCure.Magic = true
-                if IsPlayerSpell(388874) then --Improved Detox 
-                    canCure.Poison = true
-                    canCure.Disease = true
-                end
-            end
-            if IsSpellKnown(115310) then --Revival
-                canCure.Magic = true
-                canCure.Poison = true
-                canCure.Disease = true
-            end
-            if IsSpellKnown(115310) then --Restoral
-                canCure.Poison = true
-                canCure.Disease = true
-            end
-        end,
-        ["PALADIN"] = function()
-            if IsSpellKnown(213644) then --Cleanse Toxins
-                canCure.Poison = true
-                canCure.Disease = true
-            end
-            if IsSpellKnown(4987) then --Cleanse
-                canCure.Magic = true
-                if IsPlayerSpell(393024) then --Improved Cleanse
-                    canCure.Poison = true
-                    canCure.Disease = true
-                end
-            end
-            if IsSpellKnown(1022) then --Blessing of Protection
-                canCure.Bleed = true
-            end
-        end,
-        ["PRIEST"] = function()
-            if IsSpellKnown(527) then --Purify
-                canCure.Magic = true
-                if IsPlayerSpell(390632) then --Improved Purify
-                    canCure.Disease = true
-                end
-            end
-            if IsSpellKnown(213634) then --Purify Disease
-                canCure.Disease = true
-            end
-            if IsSpellKnown(32375) then --Mass Dispel
-                canCure.Magic = true
-            end
-        end,
-        ["SHAMAN"] = function()
-            if IsSpellKnown(51886) then --Cleanse Spirit
-                canCure.Curse = true
-            end
-            if IsSpellKnown(77130) then --Purify Spirit
-                canCure.Magic = true
-                if IsPlayerSpell(383016) then --Improved Purify Spirit
-                    canCure.Curse = true
-                end
-            end
-            if IsSpellKnown(383013) then --Poision Cleansing Totem
-                canCure.Poison = true
-            end
-        end,
-        ["WARLOCK"] = function()
-            if IsSpellKnown(89808, true) then --Singe Magic
-                canCure.Magic = true
-            end
-        end,
-        ["EVOKER"] = function()
-            if IsSpellKnown(374251) then --Cauterizing Flame
-                canCure.Curse = true
-                canCure.Poison = true
-                canCure.Disease = true
-                canCure.Bleed = true
-            end
-            if IsSpellKnown(365585) then --Expunge 
-                canCure.Poison = true
-            end
-            if IsSpellKnownOrOverridesKnown(360823) then --Naturalize 
-                canCure.Magic = true
-                canCure.Poison = true
-            end
-        end,
-        ["DEMONHUNTER"] = function()
-            if IsSpellKnown(205604) then --Reverse Magic
-                canCure.Magic = true
-            end
-        end,
-        ["HUNTER"] = function()
-            if IsSpellKnown(212640) then --Mending Bandage
-                canCure.Poison = true
-                canCure.Disease = true
-            end
-        end,
-    }
-    if dispelAbilities[playerClass] then
-        dispelAbilities[playerClass]()
-    end
 end
 
 function module:SetUpdateHealthColor()
@@ -349,25 +170,63 @@ function module:GetDebuffColors()
     debuffColors.Bleed = dbObj.Bleed
 end
 
-local eventFrame = nil
-function module:RegisterEvents()
-    if not eventFrame then
-        eventFrame = CreateFrame("Frame")
+function module:OnEnable()
+    self:SetUpdateHealthColor()
+    local dbObj = RaidFrameSettings.db.profile.DebuffHighlight
+    if dbObj.Config.operation_mode == 1 then
+        LCD = {}
+        LCD = LibStub("LibCanDispel-1.0")
+    else
+        local shownDispelType = {
+            ["Curse"] = dbObj.Config.Curse,
+            ["Disease"] = dbObj.Config.Disease,
+            ["Magic"] = dbObj.Config.Magic,
+            ["Poison"] = dbObj.Config.Poison,
+            ["Bleed"] = dbObj.Config.Bleed,
+        }
+        LCD = {}
+        function LCD:CanDispel(dispelName)
+            return shownDispelType[dispelName]
+        end
     end
-    eventFrame:SetScript("OnEvent", function(self, event, ...)
-        module:UpdateCurable()
+    self:GetDebuffColors()
+    self:HookFunc("CompactUnitFrame_RegisterEvents", function(frame)
+        if frame.unit:match("na") then --this will exclude nameplates and arena
+            return
+        end
+        if not UnitIsPlayer(frame.unit) then --exclude pet/vehicle frame
+            return
+        end
+        self:HookFrame(frame)                          
+    end)   
+    local onUpdateHealthColor
+    if RaidFrameSettings.db.profile.HealthBars.Colors.statusbarmode == 3 then 
+        onUpdateHealthColor = function(frame) 
+            if blockColorUpdate[frame] then
+                updateColor(frame)
+            else
+                updateHealthColor(frame)
+            end
+        end
+    else
+        onUpdateHealthColor = function(frame) 
+            if blockColorUpdate[frame] then
+                updateColor(frame)
+            end
+        end
+    end
+    --[[
+        CompactUnitFrame_UpdateHealthColor checks the current healthbar color value and restores it to the designated color if it differs from it.
+        If this happens while the frame has a debuff color, we will need to update it again.
+    ]]--
+    self:HookFuncFiltered("CompactUnitFrame_UpdateHealthColor", onUpdateHealthColor)
+    RaidFrameSettings:IterateRoster(function(frame)
+        self:HookFrame(frame)
+        updateAurasFull(frame)
     end)
-    eventFrame:RegisterEvent("TRAIT_CONFIG_UPDATED") 
-    eventFrame:RegisterEvent("LEARNED_SPELL_IN_TAB") 
-    eventFrame:RegisterUnitEvent("PLAYER_SPECIALIZATION_CHANGED", "player")
-    if playerClass == "WARLOCK" then
-        eventFrame:RegisterUnitEvent("UNIT_PET", "player")
-    end
 end
 
-function module:UnregisterEvents()
-    if not eventFrame then
-        return
-    end
-    eventFrame:UnregisterAllEvents()
+function module:OnDisable()
+    self:DisableHooks()
 end
+

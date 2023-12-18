be43ae2aebe1b8a02b6f0409b7dc7c8b1c2f1e5f
Slothpala <slothpala@gmail.com>
2023-12-18 19:00:00 +0100

    Initial Commit

---
+++ Init.lua
@@ -0,0 +1,7 @@
+local _, addonTable = ...
+
+addonTable.isRetail = true
+
+addonTable.texturePaths = {
+    PortraitIcon = "Interface\\AddOns\\RaidFrameSettings\\Textures\\Icon\\Icon.tga",
+}
---
+++ Init_Wrath.lua
@@ -0,0 +1,8 @@
+local _, addonTable = ...
+
+addonTable.isClassic = true
+addonTable.isWrath = true
+
+addonTable.texturePaths = {
+    PortraitIcon = "Interface\\AddOns\\RaidFrameSettings\\Textures\\Icon\\Icon_Circle.tga",
+}

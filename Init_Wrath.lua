5e370eed32b5cf2d0d018b7e024ab29acffe6590
Slothpala <slothpala@gmail.com>
2024-01-23 13:25:43 +0100

    fix for No available options menu #14

--- Init_Wrath.lua
+++ Init_Wrath.lua
@@ -6,3 +6,10 @@ addonTable.isWrath = true
 addonTable.texturePaths = {
     PortraitIcon = "Interface\\AddOns\\RaidFrameSettings\\Textures\\Icon\\Icon_Circle.tga",
 }
+
+addonTable.playableHealerClasses = {
+    [1] = "PRIEST", 
+    [2] = "PALADIN", 
+    [3] = "SHAMAN", 
+    [4] = "DRUID", 
+}

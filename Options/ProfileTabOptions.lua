1ed08d0bf6cddcd423203e7e09856712e8c79e02
Slothpala <slothpala@gmail.com>
2024-07-22 17:28:00 +0200

    replaced ace options table with a custom one

--- Options/ProfileTabOptions.lua
+++ Options/ProfileTabOptions.lua
@@ -1,8 +1,30 @@
 --[[Created by Slothpala]]--
+--[[
+  The profile creation is inspired by the default AceDB profile options to give a familiar feel to the user.
+  But I had to make some changes to better handle the group type profile options.
+]]
+
 local addonName, addonTable = ...
 local addon = addonTable.addon
 local L = LibStub("AceLocale-3.0"):GetLocale(addonName)
 local ACD = LibStub("AceConfigDialog-3.0")
+
+local class_id = select(3, UnitClass("player"))
+local profiles = {}
+
+local function get_profiles(info)
+  local no_current = info.arg == "no_current"
+  local current_profile = addon.db:GetCurrentProfile()
+  local profile_list = {}
+  for k, v in pairs(addon.db:GetProfiles(profiles)) do
+    if no_current and v == current_profile then
+      -- skip
+    else
+      profile_list[k] = v
+    end
+  end
+  return profile_list
+end
 
 local options = {
   name = L["profiles_options_tab_name"],
@@ -10,7 +32,94 @@ local options = {
   type = "group",
   childGroups = "tab",
   args = {
-    -- Order 1 = Ace Profile tab
+    profile_creation = {
+      order = 1,
+      name = function()
+        local current_profile = addon.db:GetCurrentProfile()
+        return L["profile_creation_title"] .. "( |cff39FF14" .. current_profile .. "|r )"
+      end,
+      type = "group",
+      inline = true,
+      args = {
+        reset_profile_btn = {
+          order = 0,
+          name = L["reset_profile_btn_name"],
+          desc = L["reset_profile_btn_desc"],
+          type = "execute",
+          func = function()
+            addon.db:ResetProfile()
+          end
+        },
+        new_profile_desc = {
+          order = 1,
+          name = L["new_profile_desc_name"],
+          type = "description",
+          fontSize = "medium",
+        },
+        new_profile = {
+          order = 2,
+          name = L["new_profile_name"],
+          desc = L["new_profile_desc"],
+          type = "input",
+          get = function()
+            return ""
+          end,
+          set = function(_, input)
+            -- Create the new profile
+            addon.db:SetProfile(input)
+            -- Set the current group type profile to the newly created profile
+            local current_spec_id = GetSpecialization()
+            local current_spec_name = select(2, GetSpecializationInfoForClassID(class_id, current_spec_id))
+            local group_type = addon:GetGroupType()
+            addon.db.global[current_spec_name][group_type .. "_profile"] = input
+          end,
+        },
+        copy_profile_desc = {
+          order = 3,
+          name = L["copy_profile_desc_name"],
+          type = "description",
+          fontSize = "medium",
+        },
+        copy_profile = {
+          disabled = function()
+            local profile_list = get_profiles({arg = "no_current"})
+            return not next(profile_list)
+          end,
+          order = 4,
+          name = L["copy_profile_name"],
+          desc = L["copy_profile_desc"],
+          type = "select",
+          values = get_profiles,
+          set = function(_, info)
+            addon.db:CopyProfile(profiles[info])
+          end,
+          arg = "no_current",
+        },
+        delete_profile_desc = {
+          order = 5,
+          name = L["delete_profile_desc_name"],
+          type = "description",
+          fontSize = "medium",
+        },
+        delete_profile = {
+          disabled = function()
+            local profile_list = get_profiles({arg = "no_current"})
+            return not next(profile_list)
+          end,
+          order = 6,
+          name = L["delete_profile_name"],
+          desc = L["delete_profile_desc"],
+          type = "select",
+          values = get_profiles,
+          set = function(_, info)
+            addon.db:DeleteProfile(profiles[info])
+          end,
+          arg = "no_current",
+          confirm = true,
+          confirmText = L["delete_profile_confirm"],
+        },
+      },
+    },
     group_type_profiles = {
       order = 2,
       name = "Group Profiles",
@@ -47,7 +156,7 @@ local options = {
           type = "execute",
           func = function()
             local pop_up_frame = addon:GetPopUpFrame()
-            pop_up_frame.title:SetText("Export")
+            pop_up_frame.title:SetText("Export" .. ": |cff39FF14" .. addon.db:GetCurrentProfile() .. "|r")
             pop_up_frame:Show()
             ACD:Open("RaidFrameSettings_Export_Profile_Options_PopUp", pop_up_frame.container)
           end
@@ -57,9 +166,7 @@ local options = {
   },
 }
 
-local profiles = {}
 
-local class_id = select(3, UnitClass("player"))
 for i=1, GetNumSpecializationsForClassID(class_id) do
   local _, spec_name, _, spec_icon = GetSpecializationInfoForClassID(class_id, i)
 
@@ -75,6 +182,7 @@ for i=1, GetNumSpecializationsForClassID(class_id) do
   local function set_spec_group_profile(info, value)
     addon.db:GetProfiles(profiles)
     addon.db.global[spec_name][info[#info]] = profiles[value]
+    -- if it matches the current group type and spec load the profile
     if string.find(info[#info], addon:GetGroupType()) then
       local current_spec_id = GetSpecialization()
       local current_spec_name = select(2, GetSpecializationInfoForClassID(class_id, current_spec_id))
@@ -194,10 +302,6 @@ for i=1, GetNumSpecializationsForClassID(class_id) do
 end
 
 function addon:GetProfileTabOptions()
-  local profile_options = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
-  profile_options.order = 1
-  profile_options.inline = true
-  options.args.profiles = profile_options
   return options
 end
 

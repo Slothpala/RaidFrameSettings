5af9b59b881720118069799ec690661821c2ae0c
Slothpala <slothpala@gmail.com>
2024-07-27 18:37:53 +0200

    Util added

---
+++ Utils/DB_Transition.lua
@@ -0,0 +1,21 @@
+--[[Created by Slothpala]]--
+--[[
+  This is used when i have to make db changes and is alway loaded right after LoadDataBase.
+]]
+local _, addonTable = ...
+local addon = addonTable.addon
+
+-- @TODO: Remove after a few updates.
+local function convert_buff_highlight_auras()
+  for spell_id, value in next, addon.db.profile.BuffHighlight.auras do
+    if type(value) ~= "table" then
+      addon.db.profile.BuffHighlight.auras[spell_id] = {
+        own_only = true
+      }
+    end
+  end
+end
+
+function addon:CheckDatabase()
+  convert_buff_highlight_auras()
+end

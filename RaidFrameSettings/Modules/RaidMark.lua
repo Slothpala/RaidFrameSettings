dfcb78f67f9b7c2f39a368cc93ead6f25712de16
Slothpala <slothpala@gmail.com>
2025-12-19 16:34:49 +0100

    Improved performance by moving to one event handler for FRAME_ENV_CREATED

--- RaidFrameSettings/Modules/RaidMark.lua
+++ RaidFrameSettings/Modules/RaidMark.lua
@@ -13,23 +13,23 @@ local module = addon:CreateModule("RaidMark")
 function module:OnEnable()
   local db_obj = addon.db.profile.module_data.RaidMark
 
+  local function update_raid_mark(cuf_frame)
+    local unit = cuf_frame.unit
+    if not UnitExists(unit) then
+      return
+    end
+    local raid_target_index = GetRaidTargetIndex(unit)
+    local texture = cuf_frame.RFS_FrameEnvironment.module_data.RaidMark.texture
+    if raid_target_index then
+      SetRaidTargetIconTexture(texture, raid_target_index)
+      texture:Show()
+    else
+      texture:Hide()
+    end
+  end
+
   local function create_or_update_raid_marker(cuf_frame)
     local frame_env = cuf_frame.RFS_FrameEnvironment
-
-    local function update_raid_mark()
-      local unit = cuf_frame.unit
-      if not UnitExists(unit) then
-        return
-      end
-      local raid_target_index = GetRaidTargetIndex(unit)
-      local texture = frame_env.module_data.RaidMark.texture
-      if raid_target_index then
-        SetRaidTargetIconTexture(texture, raid_target_index)
-        texture:Show()
-      else
-        texture:Hide()
-      end
-    end
 
     if not frame_env.module_data.RaidMark then
       local raid_mark_data = {}
@@ -39,7 +39,6 @@ function module:OnEnable()
       texture:Hide()
       raid_mark_data.texture = texture
       frame_env.module_data.RaidMark = raid_mark_data
-      frame_env:register_event_callback("RaidMark", "RAID_TARGET_UPDATE", update_raid_mark)
     end
 
     local texture = frame_env.module_data.RaidMark.texture
@@ -48,9 +47,13 @@ function module:OnEnable()
     texture:SetSize(28, 28)
     texture:SetScale(db_obj.scale)
 
-    update_raid_mark()
+    update_raid_mark(cuf_frame)
   end
 
+  self:RegisterForEvent("RAID_TARGET_UPDATE", function()
+    private.IterateRoster(update_raid_mark, true)
+    private.IterateMiniRoster(update_raid_mark, true)
+  end)
 
   CR.RegisterCallback("FRAME_ENV_CREATED", create_or_update_raid_marker)
   private.IterateRoster(create_or_update_raid_marker, true)

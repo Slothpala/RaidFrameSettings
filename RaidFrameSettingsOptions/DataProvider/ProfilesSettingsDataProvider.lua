c43f83ce661800c7c5e048fe27c7900d9fe25034
Slothpala <slothpala@gmail.com>
2026-01-23 17:20:44 +0100

    Testing profile list dropdown.

--- RaidFrameSettingsOptions/DataProvider/ProfilesSettingsDataProvider.lua
+++ RaidFrameSettingsOptions/DataProvider/ProfilesSettingsDataProvider.lua
@@ -14,11 +14,31 @@ local options = {
       type = "title",
       settings_text = L["profiles_header_1"] .. " ( |cff39FF14" .. main_addon.db:GetCurrentProfile() .. "|r )",
     },
+    profile_list = {
+      order = 2,
+      type = "dropdown",
+      settings_text = L["option_health_text_display_mode"],
+      db_obj = data_base.profile.module_data,
+      db_key = "health_text_display_mode",
+      options = {},
+      associated_modules = {
+        "CVar_raidFramesHealthText",
+        "CVar_pvpFramesHealthText",
+      },
+    },
   },
 }
 
 data_manager.get_data_provider = function ()
   local data_provider = CreateTreeDataProvider()
+
+  -- Feed the profiles dropdown.
+  table.wipe(options[1].profile_list.options)
+  local ace_db_profiles = main_addon.db:GetProfiles()
+  for _, profile_name in pairs(ace_db_profiles) do
+    local entry_tbl = {profile_name, profile_name}
+    table.insert(options[1].profile_list.options, entry_tbl)
+  end
 
   for _, category in ipairs(options) do
     local order_tbl = {}
